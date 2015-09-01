d7511ed5e1c433c1bcfa66766c24e91ab44af995
Alexey Skobkin <x@skobkin.ru>
2015-09-02 02:59:16 +0300

    Base for clickable Markdown links.

--- app/src/main/java/org/itishka/pointim/utils/TextLinkify.java
+++ app/src/main/java/org/itishka/pointim/utils/TextLinkify.java
@@ -7,6 +7,7 @@ import android.text.style.StyleSpan;
 
 import org.itishka.pointim.widgets.spans.PostClickableSpan;
 import org.itishka.pointim.widgets.spans.UserClickableSpan;
+import org.itishka.pointim.widgets.spans.MarkdownLinkClickableSpan;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,6 +18,7 @@ import java.util.regex.Pattern;
 public class TextLinkify {
     private static final Pattern nickPattern = Pattern.compile("(?<=^|[:(>\\s])@([\\w-]+)");
     private static final Pattern postNumberPattern = Pattern.compile("(?<=^|[:(>\\s])#(\\w+)(?>/(\\d+))?");
+    private static final Pattern markdownLinkPattern = Pattern.compile("(?<=^|[:(>\\s])\\[([\\w\\W]+)\\]\\(([\\w\\W]+)\\)");
 
     public static Spannable addLinks(String text) {
         Spannable spannable = new SpannableString(text);
@@ -41,6 +43,16 @@ public class TextLinkify {
             StyleSpan b = new StyleSpan(android.graphics.Typeface.BOLD);
             text.setSpan(b, m.start(), m.end(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
             PostClickableSpan span = new PostClickableSpan(m.group(1), m.group(2));
+            text.setSpan(span, m.start(), m.end(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        }
+        return text;
+    }
+
+    public static Spannable markMarkdownLinks(Spannable text) {
+        Matcher m = = markdownLinkPattern.matcher(text);
+        while (m.find()) {
+            MarkdownLinkClickableSpan span = new MarkdownLinkClickableSpan(m.group(2), m.group(1));
+            // TODO: 9/2/15 Add in-text replacement of markdown link definition "[some text](http://link/)" => "some text"
             text.setSpan(span, m.start(), m.end(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
         }
         return text;
