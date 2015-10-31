eb7eadc4e20b451a9ad1a13e84a5fefc7d416744
Tishka17 <tishka17@mail.ru>
2015-10-31 22:44:55 +0300

    request permissions

--- app/src/main/java/org/itishka/pointim/activities/NewPostActivity.java
+++ app/src/main/java/org/itishka/pointim/activities/NewPostActivity.java
@@ -1,9 +1,13 @@
 package org.itishka.pointim.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
@@ -19,6 +23,13 @@ public class NewPostActivity extends ConnectedActivity {
     public static final String EXTRA_TAGS = "tags";
     public static final String EXTRA_PRIVATE = "private";
     public static final String EXTRA_RESULT_POST = "post";
+    private static final int PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        setupFragment();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,43 +39,55 @@ public class NewPostActivity extends ConnectedActivity {
         if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
-        if (savedInstanceState == null) {
-            Fragment fragment = null;
-            Intent intent = getIntent();
-            String action = intent.getAction();
-            String type = intent.getType();
-            Log.d("NPA", "type: " + type);
-            if (Intent.ACTION_SEND.equals(action) && type != null) {
-                if ("text/plain".equals(type)) {
-                    fragment = NewPostFragment.newInstance(intent.getStringExtra(Intent.EXTRA_TEXT));
-                } else if (type.startsWith("image/")) {
-                    fragment = NewPostFragment.newInstance((Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM), type);
-                }
-            } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
-                if (type.startsWith("image/")) {
-                    fragment = NewPostFragment.newInstance(intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM), type);
-                }
-            } else {
-                String id = getIntent().getStringExtra(EXTRA_ID);
-                String text = getIntent().getStringExtra(EXTRA_TEXT);
-                String[] tags = getIntent().getStringArrayExtra(EXTRA_TAGS);
-                boolean isPrivate = getIntent().getBooleanExtra(EXTRA_PRIVATE, false);
-                if (!TextUtils.isEmpty(id)) {
-                    fragment = NewPostFragment.newInstanceForEdit(id, text, tags, isPrivate);
-                } else {
-                    fragment = NewPostFragment.newInstance(isPrivate);
-                }
-                if (!TextUtils.isEmpty(id)) {
-                    getSupportActionBar().setTitle("#" + id);
-                }
-            }
-            if (fragment == null)
-                fragment = NewPostFragment.newInstance(false);
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, fragment)
-                    .commit();
+        if (savedInstanceState == null) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+            setupFragment();
         }
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupFragment() {
+        Fragment fragment = null;
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        Log.d("NPA", "type: " + type);
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                fragment = NewPostFragment.newInstance(intent.getStringExtra(Intent.EXTRA_TEXT));
+            } else if (type.startsWith("image/")) {
+                fragment = NewPostFragment.newInstance((Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM), type);
+            }
+        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                fragment = NewPostFragment.newInstance(intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM), type);
+            }
+        } else {
+            String id = getIntent().getStringExtra(EXTRA_ID);
+            String text = getIntent().getStringExtra(EXTRA_TEXT);
+            String[] tags = getIntent().getStringArrayExtra(EXTRA_TAGS);
+            boolean isPrivate = getIntent().getBooleanExtra(EXTRA_PRIVATE, false);
+            if (!TextUtils.isEmpty(id)) {
+                fragment = NewPostFragment.newInstanceForEdit(id, text, tags, isPrivate);
+            } else {
+                fragment = NewPostFragment.newInstance(isPrivate);
+            }
+            if (!TextUtils.isEmpty(id)) {
+                getSupportActionBar().setTitle("#" + id);
+            }
+        }
+        if (fragment == null)
+            fragment = NewPostFragment.newInstance(false);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, fragment)
+                .commit();
     }
 }
