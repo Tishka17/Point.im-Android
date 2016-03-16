09b16097c57b2050f174535fd5cc3cb16d54681d
Tishka17 <tishka17@mail.ru>
2016-03-16 23:02:39 +0300

    explode on exit imageview

--- app/src/main/java/org/itishka/pointim/activities/ImageViewActivity.java
+++ app/src/main/java/org/itishka/pointim/activities/ImageViewActivity.java
@@ -1,15 +1,12 @@
 package org.itishka.pointim.activities;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.transition.Explode;
 
 import org.itishka.pointim.R;
 import org.itishka.pointim.fragments.ImageListViewFragment;
-import org.itishka.pointim.fragments.ImageViewFragment;
 
 public class ImageViewActivity  extends ConnectedActivity {
     public static final String EXTRA_URLS = "urls";
@@ -17,6 +14,9 @@ public class ImageViewActivity  extends ConnectedActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setExitTransition(new Explode());
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
         if (savedInstanceState == null) {
